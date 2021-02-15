e6cca3a628412b9c392dce231dbc8708e385d7e8
Umay Serap KOÇ <Umay404@users.github.com>
2021-02-16 00:28:50 +0300

    1

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{ //Sistemi veri tabanı nesleri ile çalıştırmak için;
+   //Generic Constraint :Generic kısıt, gönderilecek neslerikısıtlama
+   //class:Reference
+   //IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+   //new():new'lenebilir olmalı,interface new lenemediği için onları da sınırladık
+   public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        
+        List<T> GetAll(Expression<Func<T,bool>>filter=null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+       
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context:Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        //Db yerini göstermek için
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Sql server kullanacağım ve nasıl bağlanacağını belirmeliyim.
+            optionsBuilder.UseSqlServer(@"Server=LENOVO-PC\SQL2019;Database-Northwind;Trusted_Connection=true");
+        }
+        //kullandığımız tabloları tanımladık
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
